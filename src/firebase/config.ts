e2b5a3214ddaf57dac2a33aee23a9ab18287c285
Modou Sall <modousall1@gmail.com>
2026-02-24 00:58:43 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2004607225-f6a14",
+  "appId": "1:869931588586:web:138e7aaff905d5fdf37093",
+  "apiKey": "AIzaSyC1rCtbDcbV3slNAw4LSgIGxD1yMfNQ6Lo",
+  "authDomain": "studio-2004607225-f6a14.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "869931588586"
+};
